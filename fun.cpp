6b2b1083dbfce700a081217ebe670ac3fbb7f0d0
TinkerSo <113651019+TinkerSo@users.noreply.github.com>
2023-01-26 19:38:47 -0500

    Add files via upload

---
+++ fun.cpp
@@ -0,0 +1,9 @@
+#include "fun.h"
+/* your includes here */
+
+unsigned int fun(unsigned int x, unsigned int y) {
+	/* your code here */
+	return 0; /* don't forget to change the return value */
+}
+
+/* your helper function here if you have any */
